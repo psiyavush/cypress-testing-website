798f38ea2d00c0decbff3267d72071b40220d144
Sievush Park <siyavush@gmail.com>
2024-09-06 16:32:01 +0300

    upgrade article

--- src/components/ArticleDetail/ArticleDetail.jsx
+++ src/components/ArticleDetail/ArticleDetail.jsx
@@ -82,10 +82,10 @@ function ArticleDetail(props) {
                             )}
                           </Col>
                           <Col className={styles.authorDateName}>
-                            <div className={styles.authorName}>
+                            <div className={styles.authorName} cy-data="username">
                               {article?.author.username || <Skeleton />}
                             </div>
-                            <p className={styles.date}>
+                            <p className={styles.date} data-cy="date">
                               {article?.createdAt ? (
                                 new Date(article?.createdAt).toLocaleDateString(
                                   "en-US",
@@ -190,10 +190,10 @@ function ArticleDetail(props) {
                       handleProfileClick(article?.author?.username)
                     }
                   >
-                    <div className={styles.authorName2}>
+                    <div className={styles.authorName2}{...{ 'data-cy': 'username' }}>
                       {article?.author.username}
                     </div>
-                    <p className={styles.date}>
+                    <p className={styles.date} {...{ 'data-cy': 'date' }}>
                       {article?.createdAt
                         ? new Date(article?.createdAt).toLocaleDateString(
                             "en-US",
